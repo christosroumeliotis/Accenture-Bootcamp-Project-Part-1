e8c8867966f744bb3244d4198ae617c5661f0e54
Dimitra <>
2024-04-14 17:54:11 +0300

    Add constants and random generators

---
+++ src/main/java/org/example/utilities/Constants.java
@@ -0,0 +1,16 @@
+package org.example.utilities;
+
+public class Constants {
+    public static final int NUMBER_OF_INSUREDS = 15;
+    public static final int NUMBER_OF_VACCINATION_CENTERS = 2;
+    public static final int NUMBER_OF_DOCTORS = 4;
+    public static final int NUMBER_OF_TIMESLOT = 20;
+    public static final int NUNBER_OF_RESERVATIONS = 8;
+    public static final int NUMBER_OF_VACCINATIONS = 6;
+
+    public static final String[] FIRST_NAMES = {"John", "Dimitra", "Michael", "Sophia", "Paris", "Oliver", "George", "Anna", "James", "Helen"};
+    public static final String[] LAST_NAMES = {"Papageorgiou", "Tsaklidis", "Fotiadis", "Andriotis", "Paraskeva", "Dionisiou", "Theodorou", "Arvaniti", "Papadopoulou", "Kefala"};
+
+    public static final String[] VACCINATION_CENTER_CODE = {"THESS01", "THESS02", "ATH01", "ATH02"};
+    public static final String[] VACCINATION_CENTER_ADDRESS = {"Tsimiski 1", "EGNATIA 1", "Ermou 1", "Stadiou 1"};
+}
